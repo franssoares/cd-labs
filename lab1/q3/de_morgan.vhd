e07f6824e40428e33a446be2bdcbcc300ba3bb08
franssoares <xflsn100@gmail.com>
2023-04-11 15:57:45 -0300

    editado: linha de codigo

--- lab1/q3/de_morgan.vhd
+++ lab1/q3/de_morgan.vhd
@@ -7,10 +7,10 @@ end de_morgan;
 
 architecture main of de_morgan is
 begin
-         --1ª lei de De Morgan
+         --1th lei de De Morgan
          w <= not(a and b);
          x <= not(a) or not(b);
-         --2ª lei de De Morgan
+         --2th lei de De Morgan
          y <= not(a or b);
          z <= not(a) and not(b);
 end architecture main;
